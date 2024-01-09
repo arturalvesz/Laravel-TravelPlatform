9451ce0e13c738b6652cbdfac1edfca8bea17951
arturalves <arturalvesz77@gmail.com>
2024-01-09 17:00:59 +0000

    modified:   tests/Unit/loginTest.php

--- tests/Unit/loginTest.php
+++ tests/Unit/loginTest.php
@@ -8,8 +8,8 @@ use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
-    private const VALID_PASSWORD = '12345678';
-    private const INVALID_PASSWORD = 'pwd123213';
+    private const VALID_PASS = '12345678';
+    private const INVALID_PASS = 'pwd123213';
     private const VALID_EMAIL = 'test@hotmail.com';
     private const INVALID_EMAIL = 'test@12345.com';
 
@@ -18,9 +18,9 @@ class LoginTest extends TestCase
      */
     public function testPasswordMatching()
     {
-        $this->assertEquals(self::VALID_PASSWORD, self::VALID_PASSWORD);
-        $this->assertGreaterThanOrEqual(8, strlen(self::VALID_PASSWORD));
-        $this->assertLessThanOrEqual(20, strlen(self::VALID_PASSWORD));
+        $this->assertEquals(self::VALID_PASS, self::VALID_PASS);
+        $this->assertGreaterThanOrEqual(8, strlen(self::VALID_PASS));
+        $this->assertLessThanOrEqual(20, strlen(self::VALID_PASS));
     }
 
     /**
@@ -28,11 +28,11 @@ class LoginTest extends TestCase
      */
     public function testPasswordNotMatching()
     {
-        $this->assertNotEquals(self::VALID_PASSWORD, self::INVALID_PASSWORD);
-        $this->assertLessThanOrEqual(20, strlen(self::VALID_PASSWORD));
-        $this->assertGreaterThanOrEqual(8, strlen(self::VALID_PASSWORD));
-        $this->assertLessThanOrEqual(20, strlen(self::INVALID_PASSWORD));
-        $this->assertGreaterThanOrEqual(8, strlen(self::INVALID_PASSWORD));
+        $this->assertNotEquals(self::VALID_PASS, self::INVALID_PASS);
+        $this->assertLessThanOrEqual(20, strlen(self::VALID_PASS));
+        $this->assertGreaterThanOrEqual(8, strlen(self::VALID_PASS));
+        $this->assertLessThanOrEqual(20, strlen(self::INVALID_PASS));
+        $this->assertGreaterThanOrEqual(8, strlen(self::INVALID_PASS));
     }
 
     /**
