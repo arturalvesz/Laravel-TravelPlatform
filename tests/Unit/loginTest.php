39d5a85f24a8e071063a2372b24c1dbaa039f634
arturalves <arturalvesz77@gmail.com>
2024-01-09 18:34:05 +0000

    update tests

--- tests/Unit/loginTest.php
+++ tests/Unit/loginTest.php
@@ -56,6 +56,6 @@ class LoginTest extends TestCase
         $isValidLength = strlen(self::INVALID_EMAIL) >= 8 && strlen(self::INVALID_EMAIL) <= 255;
 
         // Assert that either the email is not valid or its length is outside the allowed range
-        $this->assertTrue($isInvalidEmail || !$isValidLength);
+        $this->assertFalse($isInvalidEmail || !$isValidLength);
     }
 }
